e5af66d508821b11a4509a1e2ad01aa3dac01e76
HeJae Moon <mhj2973@naver.com>
2023-10-21 22:49:55 +0900

    수박게임 스폰세팅2

--- MoaDoa_Project/Assets/Scripts/WaterMelon/WaterMelon_Spawnpoint.cs
+++ MoaDoa_Project/Assets/Scripts/WaterMelon/WaterMelon_Spawnpoint.cs
@@ -6,11 +6,15 @@ using UnityEngine.UI;
 // 수박게임 Spawn 위치 및 생성
 public class WaterMelon_Spawnpoint : MonoBehaviour
 {
+    [HideInInspector]
     public GameObject[] gameObjects;
 
     // UI세팅하기
+    
     public GameObject spawnObject; 
-    public GameObject waitingObject; 
+    public GameObject waitingObject;
+
+    public Transform waiting_Point;
 
     // gamemanager에서 가져올 오브젝트
     public Queue<GameObject>[] firstTwoObjects = new Queue<GameObject>[2];
@@ -77,8 +81,6 @@ public class WaterMelon_Spawnpoint : MonoBehaviour
 
     }
 
-   
-
     // firstTwoObjects[3,4] / UI[1.2] 세팅완료
     void Setting_Process()
     {
@@ -86,6 +88,9 @@ public class WaterMelon_Spawnpoint : MonoBehaviour
         spawnObject = firstTwoObjects[0].Dequeue();
         waitingObject = firstTwoObjects[1].Dequeue();
 
+        Instantiate(spawnObject, this.transform.position, Quaternion.identity);
+
+        Instantiate(waitingObject, waiting_Point.transform.position, Quaternion.identity);
            
         if (firstTwoObjects[0] == null)
         {
@@ -105,14 +110,17 @@ public class WaterMelon_Spawnpoint : MonoBehaviour
             
     }
        
-        
-    // 버튼 눌릴시 프로세스
+   
+
+   // 버튼 눌릴시 프로세스
    void Input_Proccess()
     {
         // 버튼이 떨어졌을때 
         spawnObject = waitingObject;
+        Instantiate(spawnObject, this.transform.position, Quaternion.identity);
 
         waitingObject = firstTwoObjects[0].Dequeue();
+        Instantiate(waitingObject, waiting_Point.transform.position, Quaternion.identity);
 
         // 두 번째 큐의 첫번쨰 요소를 첫 번쨰 큐로 옮깁니다
         firstTwoObjects[0].Enqueue(firstTwoObjects[1].Dequeue());
